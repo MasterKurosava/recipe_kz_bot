277238c65d4ba8526517f2ee308b5a9c86dbc4b9
Michael Kalachev <m.kalachev@whitewill.ru>
2026-01-21 19:26:25 +0500

    1

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ async def main():
     
     from middlewares.unregistered import UnregisteredUserMiddleware
     dp.message.middleware(UnregisteredUserMiddleware())
-    dp.callback_query.middleware(UnregisteredUserMiddleware())
 
     common.router.message.middleware(RoleCheckMiddleware(['admin', 'doctor', 'pharmacist']))
     common.router.callback_query.middleware(RoleCheckMiddleware(['admin', 'doctor', 'pharmacist']))
